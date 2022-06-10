99add6422144fbb975e39aaecdb827740d8642eb
pabloarshid <umarsa.birvancity@gmail.com>
2022-06-10 11:38:20 -0700

    am able to query from api now, along with displaying the data from the api call

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from intuitlib.client import AuthClient
 from intuitlib.enums import Scopes
-
+import pandas as pd
 from flask import Flask, redirect, request, session, render_template, url_for
 
-import requests
+import requests,json
 
 app = Flask(__name__)
 
@@ -23,17 +23,32 @@ scopes = [
 # Get Authorization URL
 auth_url = auth_client.get_authorization_url(scopes)
 
-
 # Defining the home page of our site
 @app.route("/", methods=['GET'])  # this sets the route to this page
 def home():
 	return redirect(auth_url)
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+@app.route('/getdata')
+def getdata():
+    base_url = 'https://sandbox-quickbooks.api.intuit.com'
+    url = "{0}/v3/company/{1}/reports/ItemSales?start_date=2015-08-01&end_date=2015-09-30".format(base_url, auth_client.realm_id)
+    auth_header = 'Bearer {0}'.format(auth_client.access_token)
+    headers = {
+        'Authorization': auth_header,
+        'Accept': 'application/json'
+    }
+    response = requests.get(url, headers=headers)
 
-@app.route('/callback')
+    print("Response = ",response)
+    print("Response Data = ",response.text)
+
+    print("Success")
+    df = pd.read_json(response.text)
+    html = df.to_html()
+
+    return html
+
+@app.route('/callback', methods=['GET'])
 def callback():
     #User is redirectred back after doing get request with auth URL
     #with this well recieve the auth code and realmid
@@ -41,8 +56,8 @@ def callback():
     state_from_request = request.args.get('state')
     auth_code = request.args.get('code')
     realmid = request.args.get('realmId')
-    print(state_from_request,auth_code,realmid)
-    return redirect(url_for('index'))
+    auth_client.get_bearer_token(auth_code, realm_id = realmid)
+    return redirect(url_for('getdata'))
 
 
 
